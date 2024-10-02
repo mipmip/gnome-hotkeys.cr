bbed5d494171297c230690487819b167d978f39d
Pim Snel <post@pimsnel.com>
2024-10-02 17:16:37 +0200

    add build package

---
+++ package.nix
@@ -0,0 +1,37 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, crystal
+, wrapGAppsHook4
+}:
+crystal.buildCrystalPackage rec {
+  pname = "myhotkeys";
+  version = "0.2.1";
+
+  src = fetchFromGitHub {
+    owner = "mipmip";
+    repo = "gnome-hotkeys.cr";
+    rev = "v${version}";
+    hash = "";
+  };
+
+  nativeBuildInputs = [ wrapGAppsHook4 ]
+    ++ lib.optionals stdenv.isDarwin [ desktopToDarwinBundle ];
+  buildInputs = [ ];
+
+  buildTargets = [ "all" ];
+  doCheck = false;
+
+  shardsFile = ./shards.nix;
+
+  #installTargets = [ "install" "install-fonts"];
+  doInstallCheck = false;
+
+  meta = with lib; {
+    description = "Fast popup window with configured hotkeys";
+    homepage = "https://github.com/mipmip/gnome-hotkeys.cr";
+    license = licenses.mit;
+    mainProgram = "myhotkeys";
+    maintainers = with maintainers; [ sund3RRR ];
+  };
+}
