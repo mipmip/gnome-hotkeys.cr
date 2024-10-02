2a1083b90caf841667a92ef5e57c8864a0515b10
Pim Snel <post@pimsnel.com>
2024-10-02 18:56:55 +0200

    add default conf options

--- package.nix
+++ package.nix
@@ -11,7 +11,7 @@
 }:
 crystal.buildCrystalPackage rec {
   pname = "myhotkeys";
-  version = "0.2.4";
+  version = "0.2.5";
 
   src = ./.;
 
--- src/modules/views/main.cr
+++ src/modules/views/main.cr
@@ -5,18 +5,31 @@ module MyHotKeys::GtkMain
     openShortcutsPopup
   end
 
+  def runOpenWindow(shortcut_file)
+    shortcutsWindowUI = makeShortcutsUI(shortcut_file)
+    shortcutsWindow = Gtk::Builder.new_from_string(shortcutsWindowUI, shortcutsWindowUI.size.to_i64)
+    scwin = Gtk::ShortcutsWindow.cast(shortcutsWindow["shortcutsWindow"])
+    scwin.close_request_signal.connect() do
+      exit(0)
+      true
+    end
+    scwin.present()
+  end
+
   def openShortcutsPopup
+
+    default_file = Path["~/.config/myhotkeys/keys.json"].expand(home: true)
+    p default_file
+
     shortcut_file = STATE.shortcut_file
     p shortcut_file
+
     if File.exists?(shortcut_file)
-      shortcutsWindowUI = makeShortcutsUI(shortcut_file)
-      shortcutsWindow = Gtk::Builder.new_from_string(shortcutsWindowUI, shortcutsWindowUI.size.to_i64)
-      scwin = Gtk::ShortcutsWindow.cast(shortcutsWindow["shortcutsWindow"])
-      scwin.close_request_signal.connect() do
-        exit(0)
-        true
-      end
-      scwin.present()
+      runOpenWindow(shortcut_file)
+
+    elsif File.exists?(default_file)
+      runOpenWindow(default_file)
+
     else
       usage
       exit(1)
@@ -24,7 +37,7 @@ module MyHotKeys::GtkMain
   end
 
   def usage
-    print "you should give a valid file name as 1st argument\n"
+    print "Error: No ~/.config/myhotkeys/keys.json or valid file name as 1st argument.\n"
   end
 
 
